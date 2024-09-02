e0f0814b8c2a8df8f7c0325ae8129044b6d6f78f
myrepo <hxmihir@gmail.com>
2024-09-02 11:25:40 +0530

    day001

---
+++ ex1.py
@@ -0,0 +1,13 @@
+num1=int(input("enter 1st number "))
+num2=int(input("enter 2nd number "))
+
+num3 = num1 + num2
+print(num3,"is the added number")
+
+num1*=num1
+print(num1,"is the square")
+
+raised=num1**num2
+print(raised,"is the power raised number of 1st to 2nd")
+
+
--- my_first_python_program.py
+++ my_first_python_program.py
@@ -1,3 +1,17 @@
 print("HelloWorld")
 print("how are you?")
 print("its been good")
+
+
+man=float(22)
+print(type(man))
+print(man) #explict
+
+ram=int(-3.8)
+print(type(ram))
+print(ram)
+
+
+
+won=complex(3+2j)
+print(type(won))
