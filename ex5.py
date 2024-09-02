eac9270529eab509e2c0054e3fd897950fece63b
myrepo <hxmihir@gmail.com>
2024-09-02 12:49:37 +0530

    day001

---
+++ ex5.py
@@ -0,0 +1,9 @@
+stock=input("enter your stock name: ")
+buy=int(input("enter your buy price: "))
+sell=int(input("enter your selling price: "))
+units=int(input("enter no. of units bought: "))
+
+profit=sell-buy
+final_profit=int(profit*units)
+print("The profit made on ",stock,"by selling",units,"units of that stock is ",final_profit)
+
