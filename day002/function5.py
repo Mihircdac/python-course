90703bbc1b704be16e3dd9720cfabb99aef898c2
myrepo <hxmihir@gmail.com>
2024-09-03 09:05:47 +0530

    day002

--- day002/function5.py
+++ day002/function5.py
@@ -1,11 +1,11 @@
-stock=input("enter your stock name: ")
-buy=int(input("enter your buy price: "))
-sell=int(input("enter your selling price: "))
-units=int(input("enter no. of units bought: "))
-
 def stock_price(stock,buy,sell,units):
     profit=sell-buy
     final_profit=(profit*units)
     print("The profit made on ",stock,"by selling",units,"units of that stock is ",final_profit)
+
+stock=input("enter your stock name: ")
+buy=int(input("enter your buy price: "))
+sell=int(input("enter your selling price: "))
+units=int(input("enter no. of units bought: "))
 
 stock_price(stock,buy,sell,units)
