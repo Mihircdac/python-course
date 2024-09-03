12625ed7e8d0529a58168d1c9e026db41105611e
myrepo <hxmihir@gmail.com>
2024-09-03 22:21:52 +0530

    practice

---
+++ Practiceday002/7divisible.py
@@ -0,0 +1,6 @@
+num=int(input("enter number: "))
+
+if num%7==0:
+    print("divisible by 7 ")
+else:
+    print("not divisible ")
---
+++ Practiceday002/evenodd.py
@@ -0,0 +1,6 @@
+num=int(input("enter your number: "))
+
+if num%2==0:
+    print("is even")
+else:
+    print("is odd")
