9d4cb935c3ebe03e1906273fac84951548243a4d
myrepo <hxmihir@gmail.com>
2024-09-04 10:51:39 +0530

    day003

---
+++ Day003/loop2.py
@@ -0,0 +1,9 @@
+cnt = 1
+while True:
+    print(cnt,end='')
+    if cnt !=10 :
+        print(',',end = '')
+    else:             
+        break
+    
+    cnt+=1
---
+++ Day003/loops1.py
@@ -0,0 +1,9 @@
+cnt=1
+while cnt <=20:
+    print(cnt,end='')
+    if cnt !=20:
+        print(',',end='')
+    cnt+=1
+    
+print(" Over")    
+    
