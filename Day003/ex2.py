e7a0a53c50da1e9dd4302a8684153818ff13a45a
myrepo <hxmihir@gmail.com>
2024-09-04 12:59:53 +0530

    day003

---
+++ Day003/ex2.py
@@ -0,0 +1,18 @@
+num = int(input("Please enter the number"))
+
+while True:
+    name=input("enter your birthdate: ")
+    if num %5==0 and num % 3 ==0 :
+            print("Fizz Buzz")
+    elif num%3 == 0 :
+            print("Fizz")
+    elif num%5 == 0 :
+            print("Buzz")
+    else:
+        print("Invalid Input")
+        
+    Tell=input("your birthdate is not good")
+    continue
+        
+     
+    
